dd18f6a1a98720f4e785482bc99887a869b17ea0
antony <ant@energizedwork.com>
2015-04-08 18:02:14 +0100

    Api call

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var Hapi = require('hapi'),
     config = require('config'),
     _ = require('lodash-node'),
     tree = require('dir-to-json'),
-    sf = require('sf');
+    sf = require('sf'),
+    imageDiff = require('image-diff'),
+    datauri = require('datauri'),
+    async = require('async');
 
 var server = new Hapi.Server();
 server.connection({
@@ -35,7 +38,7 @@ server.route({
     path: '/',
     handler: function (request, reply) {
 
-      var screenshots = tree(config.screenshots);
+      var screenshots = tree(config.screenshots+'/current');
 
       screenshots.then(function (tree) {
           reply.view('index', {screenshots: tree});
@@ -46,18 +49,74 @@ server.route({
 
 server.route({
     method: 'GET',
-    path: '/compare/{platform}/{browser}',
+    path: '/api/compare',
     handler: function (request, reply) {
 
-      var screenshots = tree(sf('{screenshots}/{platform}/{browser}', {
-        screenshots: config.screenshots,
-        platform: request.params.platform,
-        browser: request.params.browser
-      }));
+      var currentDir = config.screenshots+'/current';
+      var previousDir = config.screenshots+'/previous';
+      var current = tree(currentDir);
 
-      screenshots.then(function (tree) {
-          reply.view('index', {screenshots: thumbnails});
+      var analysis = {};
+
+      current.then(function (tree) {
+
+        _.each(tree.children, function(platform) {
+          analysis[platform.name] = {};
+          _.each(platform.children, function(browser) {
+            analysis[platform.name][browser.name] = {};
+            _.each(browser.children, function(version) {
+              analysis[platform.name][browser.name][version.name] = [];
+              _.each(version.children, function(image) {
+
+                var diff = {};
+
+                var imagePath = sf('/{platform}/{browser}/{version}/{image}', {
+                  platform: platform.name,
+                  browser: browser.name,
+                  version: version.name,
+                  image: image.name
+                });
+
+                var diffImage = '/tmp/'+image.name;
+
+                var result = imageDiff({
+                  actualImage: currentDir + imagePath,
+                  expectedImage: previousDir + imagePath,
+                  diffImage: diffImage
+                }, function (err, imagesAreSame) {
+
+                  if (err) {
+                    console.error(err);
+                    return
+                  }
+
+                  diff.name = image.name;
+                  diff.match = imagesAreSame;
+                  if (!imagesAreSame) {
+                    diff.difference = datauri(diffImage);
+                  }
+
+                  analysis[platform.name][browser.name][version.name].push(diff);
+
+                  reply(analysis);
+
+                });
+              });
+            });
+          });
+        });
+
       });
+
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/latest/{platform}/{browser}',
+    handler: function (request, reply) {
+
+      reply().code(404);
 
     }
 });
